c71bd0d9289360a84f30865c2ca3488762e2c62f
yafet-123 <yafetaddisu123@gmail.com>
2022-10-09 08:06:43 +0300

    fixing the bug

--- components/News.js
+++ components/News.js
@@ -4,29 +4,31 @@ import Link from "next/link";
 export function News() {
   return (
     <div>
-      <div className="max-w-3xl mx-auto relative">
-        <h1 className="text-center text-4xl font-serif font-medium mb-5">
+      <div className="max-w-3xl mx-auto">
+        <h1 className="text-center text-4xl font-serif font-medium mb-5 mx-20">
           NAS News
         </h1>
-        <p className="text-center font-serif text-slate-700 tracking-wide leading-6 capitalize mb-5">
+        <p className="text-center font-serif text-slate-700 tracking-wide leading-6 capitalize mb-5 mx-20">
           Keep updated by joining our mailing list. Please enter your email to
           select the newsletters you would like to subscribe to:
         </p>
-        <input
-          placeholder="Name"
-          type="text"
-          className="w-full border-slate-400 border-2 p-3 mb-3"
-        />
 
-        <input
-          placeholder="Email"
-          type="email"
-          className="w-full border-slate-400 border-2 p-3 mb-3"
-        />
+        <div className="mx-20 lg:mx-0 relative">
+          <input
+            placeholder="Name"
+            type="text"
+            className="w-full border-slate-400 border-2 p-3 mb-3"
+          />
 
-        <button className="bg-cyan-800 border py-2 px-5 absolute left-1/2 -bottom-16">
-          Submit
-        </button>
+          <input
+            placeholder="Email"
+            type="email"
+            className="w-full border-slate-400 border-2 p-3 mb-3"
+          />
+          <button className="bg-cyan-800 border py-2 px-5 absolute left-1/2 -bottom-16">
+            Submit
+          </button>
+        </div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-24 mx-20">
         <div className="news_background_one w-full h-[40rem]"></div>
